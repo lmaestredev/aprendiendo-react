24719daa4c8498f1ec29143fa92fb87927472752
luigi <luis@flixxo.com>
2024-07-02 12:47:30 -0300

    second commit

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,19 @@
+export function App () {
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/lmaestredev" alt="El avatar de lmaestredev" />
+                <div>
+                    <strong>Luis Maestre</strong>
+                    <span>@lmaestredev</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
